eecf67c38b901ee62a74bd70c0cc081e1bffc7a9
Nic Peterson <nic@peterson.to>
2021-01-13 20:26:54 -0800

    Update our menu-items to navigate withRouter HOC -- updated linkUrl for hat in directory component so it matched the lecture.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
+
 import HomePage from "./pages/homepage/homepage.component";
 
 const HatsPage = () => (
   <div>
-    <h1>HATS PAGE</h1>
+    <h1>HATS PAGE </h1>
   </div>
 );
 
@@ -14,6 +16,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
       </Switch>
     </div>
   );
